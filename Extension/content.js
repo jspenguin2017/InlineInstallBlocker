de08d82027b8c476416283e27367086b432de8b7
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-08-11 08:52:01 -0600

    properly garbage collect useless pointers

--- Extension/content.js
+++ Extension/content.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //The communication key
-const magic = "" + Math.random() + Math.random();
+const magic = "InlineInstallBlockerEvent_" + Math.random().toString(36).substring(2);
 
 //Establish communication bridge
 const pipe = chrome.runtime.connect({ name: "content" });
@@ -12,65 +12,67 @@ addEventListener(magic, (e) => {
     pipe.postMessage({ cmd: e.detail });
 });
 
-//Payload to inject
-const payload = `(() => {
-    //Temporary allow flag
-    let allowOnce = false;
+{
+    //Payload to inject
+    const payload = `(() => {
+        //Temporary allow flag
+        let allowOnce = false;
 
-    //Cache pointers to critical functions for security
-    const magic = "${magic}";
-    const dispatchEvent = window.dispatchEvent.bind(window);
-    const CustomEvent = window.CustomEvent.bind(window);
+        //Cache pointers to critical functions for security
+        const magic = "${magic}";
+        const dispatchEvent = window.dispatchEvent.bind(window);
+        const CustomEvent = window.CustomEvent.bind(window);
 
-    //Bind event handler
-    window.addEventListener(magic, (e) => {
-        switch (e.detail) {
-            case "allow once":
-                allowOnce = true;
-                break;
-            case "revoke allow once":
+        //Bind event handler
+        window.addEventListener(magic, (e) => {
+            switch (e.detail) {
+                case "allow once":
+                    allowOnce = true;
+                    break;
+                case "revoke allow once":
+                    allowOnce = false;
+                    break;
+                default:
+                    //Ignore
+                    break;
+            }
+        });
+
+        //Patch chrome.webstore.install
+        const _install = window.chrome.webstore.install.bind(window.chrome.webstore);
+        const _installStr = window.chrome.webstore.install.toString();
+        const install = (...args) => {
+            if (allowOnce) {
                 allowOnce = false;
-                break;
-            default:
-                //Ignore
-                break;
-        }
-    });
+                dispatchEvent(new CustomEvent(magic, { detail: "allow once used" }));
+                return _install(...args);
+            } else {
+                dispatchEvent(new CustomEvent(magic, { detail: "attempt blocked" }));
+            }
+        };
+        window.chrome.webstore.install = install;
 
-    //Patch chrome.webstore.install
-    const _install = window.chrome.webstore.install.bind(window.chrome.webstore);
-    const _installStr = window.chrome.webstore.install.toString();
-    const install = (...args) => {
-        if (allowOnce) {
-            allowOnce = false;
-            dispatchEvent(new CustomEvent(magic, { detail: "allow once used" }));
-            return _install(...args);
-        } else {
-            dispatchEvent(new CustomEvent(magic, { detail: "attempt blocked" }));
-        }
-    };
-    window.chrome.webstore.install = install;
+        //Patch Function.prototype.toString to prevent detection
+        const _toString = window.Function.prototype.toString;
+        const _toStringStr = window.Function.prototype.toString.toString();
+        const toString = function () {
+            if (this === install) {
+                return _installStr;
+            } else if (this === toString) {
+                return _toStringStr;
+            } else {
+                return _toString.apply(this, arguments);
+            }
+        };
+        window.Function.prototype.toString = toString;
 
-    //Patch Function.prototype.toString to prevent detection
-    const _toString = window.Function.prototype.toString;
-    const _toStringStr = window.Function.prototype.toString.toString();
-    const toString = function () {
-        if (this === install) {
-            return _installStr;
-        } else if (this === toString) {
-            return _toStringStr;
-        } else {
-            return _toString.apply(this, arguments);
-        }
-    };
-    window.Function.prototype.toString = toString;
+        //Let backround script know page script is injected
+        dispatchEvent(new CustomEvent(magic, { detail: "injected" }));
+    })();`;
 
-    //Let backround script know page script is injected
-    dispatchEvent(new CustomEvent(magic, { detail: "injected" }));
-})();`;
-
-//Inject page script
-const script = document.createElement("script");
-script.textContent = payload;
-document.documentElement.prepend(script);
-script.remove();
+    //Inject page script
+    const script = document.createElement("script");
+    script.textContent = payload;
+    document.documentElement.prepend(script);
+    script.remove();
+}
